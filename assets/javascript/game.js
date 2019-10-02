1748b22307c77ca2ce5148fc78e3953034c5d4aa
humberto24 <humberto24@gmail.com>
2019-10-02 14:08:48 -0500

    This is the latest version of the code for the game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,63 +19,72 @@ $(document).ready(function() {
     wins = 0;
     losses = 0;
 
-// This function creates a random number from 18 to 120:
-    randomNumber = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
+
+    function create(){
+
+    // This function creates a random number from 18 to 120:
+        randomNumber = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
 
 
-// This block of code sets the random number in the HTML page:
-    $("#random-number-text").text(randomNumber);
+    // This block of code sets the random number in the HTML page:
+        $("#random-number-text").text(randomNumber);
 
 
-// Here we give each button variable a random value from the buttonValues array:
-    randomSort = buttonValues.sort(function(a, b){return 0.5 - Math.random()});
+    // Here we give each button variable a random value from the buttonValues array:
+        randomSort = buttonValues.sort(function(a, b){return 0.5 - Math.random()});
 
-    button1 = randomSort[0];
-    button2 = randomSort[1];
-    button3 = randomSort[2];
-    button4 = randomSort[3];
+        button1 = randomSort[0];
+        button2 = randomSort[1];
+        button3 = randomSort[2];
+        button4 = randomSort[3];
 
-    console.log("Button 1 value: " + button1);
-    console.log("Button 2 value: " + button2);
-    console.log("Button 3 value: " + button3);
-    console.log("Button 4 value: " + button4);
+        console.log("Button 1 value: " + button1);
+        console.log("Button 2 value: " + button2);
+        console.log("Button 3 value: " + button3);
+        console.log("Button 4 value: " + button4);
 
-// Here we create a value each time a button is pressed:
-    $("#button1").on("click", function(){
-        sumText = sumText+ button1;
-        $("#sum-text").text(sumText);
-    });
+        
+    // Here we create a value each time a button is pressed:
 
-    $("#button2").on("click", function(){
-        sumText = sumText+ button2;
-        $("#sum-text").text(sumText);
-    });
+        $("#button1").on("click", function(){
+            sumText += button1;
+            $("#sum-text").text(sumText);
+        });   
 
-    $("#button3").on("click", function(){
-        sumText = sumText+ button3;
-        $("#sum-text").text(sumText);
-    });
+        $("#button2").on("click", function(){
+            sumText += button2;
+            $("#sum-text").text(sumText);
+        });
 
-    $("#button4").on("click", function(){
-        sumText = sumText+ button4;
-        $("#sum-text").text(sumText);
-    });
+        $("#button3").on("click", function(){
+            sumText += button3;
+            $("#sum-text").text(sumText);
+        });
 
-    console.log("sumText value: " + sumText);
+        $("#button4").on("click", function(){
+            sumText += button4;
+            $("#sum-text").text(sumText);
+        });
 
+        console.log(SumText);
 
-// Here we define if the player wins or loses:
+    
+        
 
-    if(sumText === randomNumber){
-        alert("You won!");
-        wins++;
-        $("#wins-text").text(wins);
+    // // Here we define if the player wins or loses:
 
+        if(sumText === randomNumber){
+            alert("You won!");
+            wins++;
+            $("#wins-text").text(wins);
+            create();
+        }
+        if(sumText > randomNumber){
+            alert("You lost! Try again.");
+            losses++;
+            $("#losses-text").text(losses);
+            create();
+        }
     }
-    if(sumText > randomNumber){
-        alert("You lost! Try again.");
-        losses++;
-        $("#losses-text").text(losses);
-    }
-
+    create();
 });
